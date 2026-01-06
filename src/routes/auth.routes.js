939f963a81630aecf11a5ccf31315af73280f525
KARTIK1111 <kartikshrikhande11@gmail.com>
2026-01-06 17:10:49 +0530

    refactor: Remove unused imports from auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,10 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
-const { authenticateUser } = require("../middlewares/auth.middleware");
-const { validationResult } = require("express-validator");
-
 
 router.post("/login", authController.login);
 router.post("/refresh-token", authMiddleware.refreshToken);
 router.get("/verify",authMiddleware.verifyToken);
 
 module.exports = router;
-
+   
